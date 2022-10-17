08973cfe2eca222ae0cbed33e80f4d485c2cd1b8
Mohammed Naji <m.naji1512@gmail.com>
2022-10-17 19:02:11 +0300

    project dashboard

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,9 @@
 
 namespace App\Http;
 
+// use App\Http\Middleware\CheckAdmin;
+
+use App\Http\Middleware\CheckAdmin;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -21,6 +24,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        // \App\Http\Middleware\CheckAdmin::class
     ];
 
     /**
@@ -63,5 +67,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'check_admin' => CheckAdmin::class
     ];
 }
